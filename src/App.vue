99d397c92962fea216963b5dea7f1a1ed95f400c
Phan Vo Minh Huy <huy.phan2210@outlook.com>
2023-06-08 13:57:02 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import Header from './components/Header.vue';
+</script>
+
+<template>
+  <Header></Header>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+</script>
+
+<template>
+    <header></header>
+</template>
+
+<style scoped>
+
+</style>
